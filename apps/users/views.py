b1094410583b6b864fe17c38898429434cffdffb
bezbak <Cemetey.2019@gmail.com>
2023-05-17 16:28:08 +0600

    add register

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from .models import User
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        profile_image = request.FILES.get("profile_image")
+        if password == confirm_password:
+            user = User.objects.create(username=username, profile_image=profile_image)
+            user.set_password(password)
+            user.save()
+            user = User.objects.get(username = username, password = password)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("index")
+    return render(request, 'register.html')
--- inst/urls.py
+++ inst/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.posts.urls")),
+    path("users/", include("apps.users.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
