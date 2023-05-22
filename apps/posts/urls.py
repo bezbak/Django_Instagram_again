f2558d16b5e6c9d9f265adb83c3ae52176b97454
bezbak <Cemetey.2019@gmail.com>
2023-05-22 15:27:07 +0600

    add create post function

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.posts.views import index
+from apps.posts.views import index, create_post
 
 urlpatterns = [
     path("", index, name = "index"),
+    path("create_post/", create_post, name = "create_post"),
 ]
