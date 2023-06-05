e97ea533449a52590993975ab1a3e629c0a79fd7
bezbak <Cemetey.2019@gmail.com>
2023-06-05 16:20:10 +0600

    add chat detal

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from apps.chat.views import chats
+from apps.chat.views import chats, chat_detail
 urlpatterns = [
-    path('', chats, name='chats')
+    path('', chats, name='chats'),
+    path('<int:id>/', chat_detail, name='chat_detail')
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Chat
+from django.shortcuts import render, redirect
+from .models import Chat, Message
 from django.db.models import Q
 # Create your views here.
 def chats(request):
@@ -8,3 +8,14 @@ def chats(request):
         'chats':chats
     }
     return render(request, 'chats.html', context)
+
+def chat_detail(request, id):
+    chat = Chat.objects.get(id=id)
+    if request.method == 'POST':
+        text =request.POST.get('text')
+        message = Message.objects.create(text = text, from_user= request.user, chat = chat)
+        return redirect('chat_detail', chat.id)
+    context = {
+        'chat':chat
+    }
+    return render(request, 'chat_detail.html', context)
